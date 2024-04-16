3a07fd6cdb69e848f5abe3f7f217c5b1a8705caa
Janko Stojkovic <75690738+Janko-Stojkovic@users.noreply.github.com>
2024-04-16 16:29:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class QuoteMachine extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      quote:'',
+      author:''
+    };
+    this.generateQuote = this.generateQuote.bind(this);
+  }
+
+  componentDidMount(){
+    this.generateQuote();
+  }
+
+  generateQuote(){
+    fetch('https://api.quotable.io/random').then(response => response.json()).then(data =>{
+      this.setState({
+        quote:data.content,
+        author:data.author
+      })
+    })
+  }
+
+  render(){
+
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${this.state.quote}" - ${this.state.author}`)}`;
+    return(
+      <div id="quote-box">
+        <span id="text">{this.state.quote}</span>
+        <h4 id="author">{this.state.author}</h4>
+        <button id="new-quote" onClick={this.generateQuote}>New Quote</button>
+        <a id="tweet-quote" href={tweetUrl} target="_blank" rel="noopener noreferrer">Tweet Quote</a>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default QuoteMachine;
